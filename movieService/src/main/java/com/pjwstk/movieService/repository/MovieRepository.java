99003f4c75ce4b9cfe8ec77eff491001a8aef7a8
s25323-pj <s25323@pjwstk.edu.pl>
2023-05-24 18:35:09 +0200

    Updated queries

--- movieService/src/main/java/com/pjwstk/movieService/repository/MovieRepository.java
+++ movieService/src/main/java/com/pjwstk/movieService/repository/MovieRepository.java
@@ -2,8 +2,17 @@ package com.pjwstk.movieService.repository;
 
 import com.pjwstk.movieService.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @Query("SELECT m FROM Movie m")
+    List<Movie> findAllMovies();
+
+    @Query("SELECT m FROM Movie m WHERE m.id = :id")
+    Movie findMovieById(@Param("id") Long id);
 }
